97db7817bb7ba13ee307c4f6afdb4f8b45420c25
doha-pc\doha <jihun22400669@gmail.com>
2026-02-05 21:48:42 +0900

    todo_repository.py -> 추가업로드

--- app/repositories/todo_repository.py
+++ app/repositories/todo_repository.py
@@ -27,21 +27,61 @@ class TodoRepository:
             conn.close()
 
     @staticmethod
-    def toggle_done(todo_id:int,member_id:int):
+    def toggle_done(todo_id: int, member_id: int):
         # 토완료 ↔ 미완료 상태 뒤집기
-        pass
+        conn = get_connection()
+        try:
+            with conn.cursor() as cursor:
+                sql = "UPDATE todos SET is_done = NOT is_done WHERE id = %s AND member_id = %s AND active = 1"
+                cursor.execute(sql, (todo_id, member_id))
+                conn.commit()
+                return cursor.rowcount
+        finally:
+            conn.close()
+
+
+
 
     @staticmethod
-    def update(todo_id: int, member_id: int):
+    def update(todo_id: int, member_id: int, title: str, memo=None, due_date=None):
         # 일정 수정
-        pass
+        conn = get_connection()
+        try:
+            with conn.cursor() as cursor:
+                sql = """UPDATE todos 
+                SET title = %s,
+                 memo = %s,
+                 due_date = %s
+                 WHERE id = %s 
+                 AND member_id = %s 
+                 AND active = 1"""
+                cursor.execute(sql, (title,memo,due_date,todo_id, member_id))
+                conn.commit()
+                return cursor.rowcount
+        finally:
+            conn.close()
 
     @staticmethod
     def soft_delete(todo_id:int,member_id:int):
-        pass
-    # 진짜 삭제가 아니라 “숨김 처리”
+        # 진짜 삭제가 아니라 “숨김 처리”
+        conn=get_connection()
+        try:
+            with conn.cursor() as cursor:
+                sql="UPDATE todos SET active = 0 WHERE id =%s AND member_id =%s AND active = 1 "
+                cursor.execute(sql,(todo_id,member_id))
+                conn.commit()
+                return cursor.rowcount
+        finally:
+            conn.close()
 
     @staticmethod
     def get_by_id(todo_id:int,member_id:int):
-        pass
-    # 수정화면용
+        # 수정화면용
+        conn=get_connection()
+        try:
+            with conn.cursor() as cursor:
+                sql ="SELECT * FROM todos WHERE id = %s AND member_id = %s AND active =1 "
+                cursor.execute(sql,(todo_id,member_id))
+                return cursor.fetchone()
+        finally:
+            conn.close()
